e75472d872f82f22b2eb4d5e4bfdff43af37f4ce
ryo-kozin <ryotaro0924h@gmail.com>
2022-03-30 16:32:47 +0900

    First commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const path = require('path')
+const http = require('http')
+const express = require('express')
+const socketio = require('socket.io')
+
+const app = express()
+const server = http.createServer(app)
+const io = socketio()
+
+const port = process.env.PORT | 3000
+const publicDirectoryPath = path.join(__dirname, '../public')
+
+app.use(express.static(publicDirectoryPath))
+
+server.listen(port, () => {
+    console.log(`Server is up on port ${port}!`);
+})
