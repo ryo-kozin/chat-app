d51b92b082cac22b4fd600ea5d18ef36ca2befd5
ryo-kozin <ryotaro0924h@gmail.com>
2022-03-30 18:27:18 +0900

    Create increment function

--- src/index.js
+++ src/index.js
@@ -5,13 +5,32 @@ const socketio = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
-const io = socketio()
+const io = socketio(server)
 
 const port = process.env.PORT | 3000
 const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
+let count = 0
+
+io.on('connection', (socket) => {
+    console.log('New WebSocket connection')
+
+    socket.emit('countUpdated', count)
+    
+    socket.on('increment', () => {
+        count++
+        
+        // Emit to a single connection 
+        // socket.emit('countUpdated', count)
+
+        // Emit to every single connected connections
+        io.emit('countUpdated', count)
+    })
+
+})
+
 server.listen(port, () => {
-    console.log(`Server is up on port ${port}!`);
+    console.log(`Server is up on port ${port}!`)
 })
